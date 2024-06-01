302ff774ac91494f953f69c9bcbc422831f6267c
Jetlonglim <133329143+Jetlonglim@users.noreply.github.com>
2024-06-02 00:45:47 +0800

    Update battlefield.cpp

--- battlefield.cpp
+++ battlefield.cpp
@@ -5,30 +5,35 @@
 #include <cstdlib>
 #include <ctime>
 
-
 using namespace std;
 
-Battlefield::Battlefield() {
+Battlefield::Battlefield()
+{
     numRobots = 0;
 }
 
-void Battlefield::addRobot(Robot* robot) {
-    if (numRobots < MAX_ROBOTS) {
+void Battlefield::addRobot(Robot *robot)
+{
+    if (numRobots < MAX_ROBOTS)
+    {
         robots[numRobots] = robot;
         numRobots++;
-    } else {
-        cout << "Battlefield is full. Cannot add more robots." <<endl;
+    }
+    else
+    {
+        cout << "Battlefield is full. Cannot add more robots." << endl;
     }
 }
 
-void Battlefield::simulateWar() {
+void Battlefield::simulateWar()
+{
     srand(time(0));
     ofstream logFile("battlefield_log.txt");
-    
 
-    for (int turn = 0; turn < 10; ++turn) { // Example: Simulate 10 turns
+    for (int turn = 0; turn < 10; ++turn)
+    { // Example: Simulate 10 turns in 1 Round
         cout << "Turn " << turn + 1 << endl;
-        logFile << "Turn " << turn + 1 ;
+        logFile << "Turn " << turn + 1;
 
         update();
         display(logFile);
@@ -36,42 +41,73 @@ void Battlefield::simulateWar() {
         logFile << endl;
         cout << endl;
 
-        // Log the battlefield status
-        logFile << "Displaying battlefield:" << endl;
-        for (int i = 0; i < numRobots; ++i) {
-        if (robots[i]->isAlive()) { // Check if the robot is alive
-                logFile << "Robot " << i + 1 << " - Position: (" 
-                        << robots[i]->getPosX() << ", " 
-                        << robots[i]->getPosY() << ")" << endl;
+        // Output the battlefield map
+        for (int i = 0; i < mapY; ++i)
+        {
+            for (int j = 0; j < mapX; ++j)
+            {
+                battlefieldMap[i][j] = '*';
+                cout << battlefieldMap[i][j];
             }
-        }       
-    }
+            cout << endl;
+        }
 
-   for (int i = 0; i < numRobots; i++) {
-        for (int j = i + 1; j < numRobots; j++) {
-            Robot *robot1 = robots[i];
-            Robot *robot2 = robots[j];
-            if (robot1->getPosX() == robot2->getPosX() && robot1->getPosY() == robot2->getPosY()) {
-                    if (ShootingRobot* sr1 = dynamic_cast<ShootingRobot*>(robot1)) {
-                        sr1->fire(mapX, mapY);
-                    }
-                    if (ShootingRobot* sr2 = dynamic_cast<ShootingRobot*>(robot2)) {
-                        sr2->fire(mapX, mapY);
-                    }
+            // Print the battlefield map
+            for (int i = 0; i < mapY; ++i)
+            {
+                for (int j = 0; j < mapX; ++j)
+                {
+                    cout << battlefieldMap[i][j];
+                }
+                cout << endl;
+            }
+
+            // Log the battlefield status
+            logFile << "Displaying battlefield:" << endl;
+            for (int i = 0; i < numRobots; ++i)
+            {
+                if (robots[i]->isAlive())
+                { // Check if the robot is alive
+                    logFile << "Robot " << i + 1 << " - Position: ("
+                            << robots[i]->getPosX() << ", "
+                            << robots[i]->getPosY() << ")" << endl;
+                }
             }
         }
-    }
+
+        for (int i = 0; i < numRobots; i++)
+        {
+            for (int j = i + 1; j < numRobots; j++)
+            {
+                Robot *robot1 = robots[i];
+                Robot *robot2 = robots[j];
+                if (robot1->getPosX() == robot2->getPosX() && robot1->getPosY() == robot2->getPosY())
+                {
+                    if (ShootingRobot *sr1 = dynamic_cast<ShootingRobot *>(robot1))
+                    {
+                        sr1->fire(mapX, mapY);
+                    }
+                    if (ShootingRobot *sr2 = dynamic_cast<ShootingRobot *>(robot2))
+                    {
+                        sr2->fire(mapX, mapY);
+                    }
+                }
+            }
+        }
+    
 
     logFile.close(); // Close the log file
 };
 
-
-void Battlefield::update() {
+void Battlefield::update()
+{
     cout << "Updating battlefield..." << endl;
-    for (int i = 0; i < numRobots; ++i) {
+    for (int i = 0; i < numRobots; ++i)
+    {
         // Example update logic
         // Here we can add logic to move robots, handle collisions, etc.
-        if (MovingRobot* mr = dynamic_cast<MovingRobot*>(robots[i])) {
+        if (MovingRobot *mr = dynamic_cast<MovingRobot *>(robots[i]))
+        {
             int newX = rand() % 3 - 1; // Random movement in X direction
             int newY = rand() % 3 - 1; // Random movement in Y direction
 
@@ -79,17 +115,18 @@ void Battlefield::update() {
             newX = max(0, std::min(newX, mapX - 1));
             newY = max(0, std::min(newY, mapX - 1));
             mr->moving(to_string(newX), to_string(newY));
+
         }
     }
 }
-void Battlefield::display(ofstream& logFile) {
+void Battlefield::display(ofstream &logFile)
+{
     cout << "Displaying battlefield:" << endl;
-    for (int i = 0; i < numRobots; ++i) {
-        cout << "Robot " << i + 1 << " - Position: (" 
-             << robots[i]->getPosX() << ", " 
+    for (int i = 0; i < numRobots; ++i)
+    {
+        cout << "Robot " << i + 1 << " - Position: ("
+             << robots[i]->getPosX() << ", "
              << robots[i]->getPosY() << ")" << endl;
-        
     }
+    
 }
-
-
