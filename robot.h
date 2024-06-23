3ea338616355228c25c0dbdc75cee2534dba3270
Jetlonglim <133329143+Jetlonglim@users.noreply.github.com>
2024-06-24 00:40:29 +0800

    Update robot.h

--- robot.h
+++ robot.h
@@ -1,4 +1,3 @@
-// robot.h
 #ifndef ROBOT_H
 #define ROBOT_H
 #include <cstdlib>
@@ -38,8 +37,12 @@ public:
         return Lives > 0;
     }
     void setAlive(bool alive) {
-            Lives = alive ? 3 : 0;
-        }
+    if (alive) {
+        Lives = 3; // Set lives to 3 when the robot is alive
+    } else {
+        Lives = 0; // Set lives to 0 when the robot dies
+    }
+}
 	void setPosX(int x) {
         posX = x;
     }
@@ -57,124 +60,13 @@ public:
 	virtual string getName() const {
         return Name;
     }
+    virtual string getType() const =0;
 		
-};
-
-class MovingRobot: public Robot{
-	public:
-        string getName() const override {return Name;}
-		MovingRobot(string name, string x, string y, int kills): Robot(name, x, y, kills){
-			int mapX = 30; // Define a variable to store the maximum x-coordinate of the map
-			int mapY = 30; // Define a variable to store the maximum y-coordinate of the map
-			if (x == "random" && y== "random"){// if input robot postion in random then it will be place randomly
-				srand(time(0)); // Seed the random number generator with the current time
-				setPosX(rand() % mapX); // Generate a random x-coordinate within the mapX range
-            	setPosY(rand() % mapY); // Generate a random y-coordinate within the mapX range
-			}
-			else if (stoi(x) <= mapX && stoi(y)<= mapY){//it will input the robot to the position
-				int posX = stoi(x);
-            	int posY = stoi(y);
-			}
-			else{
-				cout << "Invalid input. Please Try Again\n" ;// Invalid input
-			}
-		}
-
-		void moving(const string &x, const string &y) {
-        int newX = stoi(x);
-        int newY = stoi(y);
-		int posX = getPosX();
-        int posY = getPosY();
-
-        if (newX > 0) { // move right
-            posX ++;
-        } else if (newX < 0) { // move left
-            posX --;
-        } else if (newX == 0) { // move left
-            posX;
-        }
-
-        if (newY > 0) { // move upward
-            posY ++;
-        } else if (newY < 0) { // move downward
-            posY --;
-        } else if (newY == 0) { // move downward
-            posY;
-        }
-
-		setPosX(posX);
-        setPosY(posY);
-
-        cout << "New position: (" << posX << ", " << posY << ")" << endl;
+    void resetPosition(int mapX, int mapY) {
+        posX = rand() % mapX;
+        posY = rand() % mapY;
     }
+    
 };
 
-class ShootingRobot : public MovingRobot {
-public:
-    ShootingRobot(string name, string x, string y, int kills) : MovingRobot(name, x, y, kills) {}
-
-    void fire(int mapX, int mapY) {
-        srand(time(0)); // Seed the random number generator
-
-		int posX = getPosX();
-        int posY = getPosY();
-
-        cout << Name << " is shooting 3 bullets randomly around its position within a 10x10 block\n";
-
-        for (int i = 0; i < 3; ++i) {
-            int bulletX = posX + (rand() % 10 - 5);
-            int bulletY = posY + (rand() % 10 - 5);
-            cout << "Bullet " << i + 1 << " location: (" << bulletX << ", " << bulletY << ")\n";
-        }
-    }
-
-	
-};
-
-class SeeingRobot: public Robot{
-public: 
-	SeeingRobot(string name, string x, string y, int kills) : Robot(name, x, y, kills){}
-
-	void look(int offsetX, int offsetY){
-		int posX = getPosX();
-        int posY = getPosY();
-
-		cout << Name << " is looking a 9-square area centered on ("
-			<< (posX + offsetX) << ", " << (posY + offsetY) << ":\n";
-
-		for (int i = -1; i <= 1; ++i){
-			for(int j = -1; j <= 1; ++j){
-				cout << "(" << (posX + offsetX + i) << ", " << (posY + offsetY + j) << ") ";
-			}
-			cout << endl;
-		}
-	}
-};
-
-class SteppingRobot: public MovingRobot{
-public:
-    SteppingRobot(string name, string x, string y, int kills) : MovingRobot(name, x, y, kills) {}
-
-	void stepping(int newX, int newY, int enemies[][2], int &numEnemies){
-		int posX = getPosX();
-        int posY = getPosY();
-
-        setPosX(newX);
-        setPosY(newY);
-
-		// Check for enemies at the new position
-        for (int i = 0; i < numEnemies; ++i) {
-            if (enemies[i][0] == newX && enemies[i][1] == newY) {
-                AddKill();
-                // Remove the enemy by shifting the rest of the array
-                for (int j = i; j < numEnemies - 1; ++j) {
-                    enemies[j][0] = enemies[j + 1][0];
-                    enemies[j][1] = enemies[j + 1][1];
-                }
-                numEnemies--; // Decrease the count of enemies
-                break;
-            }
-        }
-	}
-};
 #endif
