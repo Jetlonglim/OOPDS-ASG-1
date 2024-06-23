f239fb0520620b2f7a6ce0e674c78861f68be6c7
Jetlonglim <133329143+Jetlonglim@users.noreply.github.com>
2024-06-24 00:38:53 +0800

    Update battlefield.cpp
    changed a lot

--- battlefield.cpp
+++ battlefield.cpp
@@ -1,132 +1,97 @@
 #include "battlefield.h"
 #include "robot.h"
-#include <iostream>
-#include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-Battlefield::Battlefield()
-{
-    numRobots = 0;
-}
-
-void Battlefield::addRobot(Robot *robot)
-{
-    if (numRobots < MAX_ROBOTS)
-    {
-        robots[numRobots] = robot;
-        numRobots++;
-    }
-    else
-    {
-        cout << "Battlefield is full. Cannot add more robots." << endl;
-    }
-}
-
-void Battlefield::simulateWar()
-{
+Battlefield::Battlefield() {
     srand(time(0));
-    ofstream logFile("battlefield_log.txt");
+}
 
-    for (int turn = 0; turn < 10; ++turn)
-    { // Example: Simulate 10 turns in 1 Round
-        cout << "Turn " << turn + 1 << endl;
-        logFile << "Turn " << turn + 1;
+void Battlefield::simulateWar(Robot* robots[], int numRobots, int mapX, int mapY) {
+    // Simulate actions for all robots
+    for (int i = 0; i < numRobots; ++i) {
+        robots[i]->move(robots, mapX, mapY);
 
-        update();
-        display(logFile);
-
-        logFile << endl;
-        cout << endl;
-
-        // Output the battlefield map
-        for (int i = 0; i < mapY; ++i)
-        {
-            for (int j = 0; j < mapX; ++j)
-            {
-                battlefieldMap[i][j] = '*';
-                cout << battlefieldMap[i][j];
+        if (RoboCop* robocop = dynamic_cast<RoboCop*>(robots[i])) {
+            robocop->look(robots, mapX, mapY);
+            robocop->fire(robots, mapX, mapY);
+            if (robocop->getKill() >= 3) {
+                TerminatorRoboCop* upgradedRobot = new TerminatorRoboCop(robocop->getName(), "random", "random", 0);
+                upgradedRobot->setAlive(true);
+                robots[i] = upgradedRobot;
             }
-            cout << endl;
-        }
-
-            // Print the battlefield map
-            for (int i = 0; i < mapY; ++i)
-            {
-                for (int j = 0; j < mapX; ++j)
-                {
-                    cout << battlefieldMap[i][j];
-                }
-                cout << endl;
+        } else if (Terminator* terminator = dynamic_cast<Terminator*>(robots[i])) {
+            terminator->look(robots, mapX, mapY);
+            terminator->step(robots, mapX, mapY);
+            if (terminator->getKill() >= 3) {
+                TerminatorRoboCop* upgradedRobot = new TerminatorRoboCop(terminator->getName(), "random", "random", 0);
+                upgradedRobot->setAlive(true);
+                robots[i] = upgradedRobot;
             }
-
-            // Log the battlefield status
-            logFile << "Displaying battlefield:" << endl;
-            for (int i = 0; i < numRobots; ++i)
-            {
-                if (robots[i]->isAlive())
-                { // Check if the robot is alive
-                    logFile << "Robot " << i + 1 << " - Position: ("
-                            << robots[i]->getPosX() << ", "
-                            << robots[i]->getPosY() << ")" << endl;
-                }
+        } else if (TerminatorRoboCop* termRoboCop = dynamic_cast<TerminatorRoboCop*>(robots[i])) {
+            termRoboCop->look(robots, mapX, mapY);
+            termRoboCop->fire(robots, mapX, mapY);
+            termRoboCop->step(robots, mapX, mapY);
+            if (termRoboCop->getKill() >= 3) {
+                UltimateRobot* upgradedRobot = new UltimateRobot(termRoboCop->getName(), "random", "random", 0);
+                upgradedRobot->setAlive(true);
+                robots[i] = upgradedRobot;
             }
-        }
-
-        for (int i = 0; i < numRobots; i++)
-        {
-            for (int j = i + 1; j < numRobots; j++)
-            {
-                Robot *robot1 = robots[i];
-                Robot *robot2 = robots[j];
-                if (robot1->getPosX() == robot2->getPosX() && robot1->getPosY() == robot2->getPosY())
-                {
-                    if (ShootingRobot *sr1 = dynamic_cast<ShootingRobot *>(robot1))
-                    {
-                        sr1->fire(mapX, mapY);
-                    }
-                    if (ShootingRobot *sr2 = dynamic_cast<ShootingRobot *>(robot2))
-                    {
-                        sr2->fire(mapX, mapY);
-                    }
-                }
+        } else if (BlueThunder* blueThunder = dynamic_cast<BlueThunder*>(robots[i])) {
+            blueThunder->fire(robots, mapX, mapY);
+            if (blueThunder->getKill() >= 3) {
+                MadBot* upgradedRobot = new MadBot(blueThunder->getName(), "random", "random", 0);
+                upgradedRobot->setAlive(true);
+                robots[i] = upgradedRobot;
             }
-        }
-    
-
-    logFile.close(); // Close the log file
-};
-
-void Battlefield::update()
-{
-    cout << "Updating battlefield..." << endl;
-    for (int i = 0; i < numRobots; ++i)
-    {
-        // Example update logic
-        // Here we can add logic to move robots, handle collisions, etc.
-        if (MovingRobot *mr = dynamic_cast<MovingRobot *>(robots[i]))
-        {
-            int newX = rand() % 3 - 1; // Random movement in X direction
-            int newY = rand() % 3 - 1; // Random movement in Y direction
-
-            // Ensure new position is within the boundaries of the map
-            newX = max(0, std::min(newX, mapX - 1));
-            newY = max(0, std::min(newY, mapX - 1));
-            mr->moving(to_string(newX), to_string(newY));
-
+        } else if (MadBot* madBot = dynamic_cast<MadBot*>(robots[i])) {
+            madBot->fire(robots, mapX, mapY);
+            if (madBot->getKill() >= 3) {
+                UltimateRobot* upgradedRobot = new UltimateRobot(madBot->getName(), "random", "random", 0);
+                upgradedRobot->setAlive(true);
+                robots[i] = upgradedRobot;
+            }
+        } else if (RoboTank* roboTank = dynamic_cast<RoboTank*>(robots[i])) {
+            roboTank->fire(robots, mapX, mapY);
+            if (roboTank->getKill() >= 3) {
+                UltimateRobot* upgradedRobot = new UltimateRobot(roboTank->getName(), "random", "random", 0);
+                upgradedRobot->setAlive(true);
+                robots[i] = upgradedRobot;
+            }
+        } else if (UltimateRobot* ultimateRobot = dynamic_cast<UltimateRobot*>(robots[i])) {
+            ultimateRobot->move(robots, mapX, mapY);
+            ultimateRobot->fire(robots, mapX, mapY);
+            ultimateRobot->step(robots, mapX, mapY);
         }
     }
 }
-void Battlefield::display(ofstream &logFile)
-{
-    cout << "Displaying battlefield:" << endl;
-    for (int i = 0; i < numRobots; ++i)
-    {
-        cout << "Robot " << i + 1 << " - Position: ("
-             << robots[i]->getPosX() << ", "
-             << robots[i]->getPosY() << ")" << endl;
+
+void Battlefield::printBattlefield(Robot* robots[], int numRobots, int mapX, int mapY) {
+    char battlefieldMap[mapY][mapX];
+
+    // Initialize the battlefield with '.'
+    for (int i = 0; i < mapY; ++i) {
+        for (int j = 0; j < mapX; ++j) {
+            battlefieldMap[i][j] = '.';
+        }
     }
-    
+
+    // Place the robots on the battlefield
+    for (int i = 0; i < numRobots; ++i) {
+        if (robots[i]->isAlive()) {
+            battlefieldMap[robots[i]->getPosY()][robots[i]->getPosX()] = 'R';
+        }
+    }
+
+    // Print the battlefield
+    for (int i = 0; i < mapY; ++i) {
+        for (int j = 0; j < mapX; ++j) {
+            cout << battlefieldMap[i][j] << ' ';
+        }
+        cout << endl;
+    }
 }
