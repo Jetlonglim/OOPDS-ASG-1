5cb17ac311bb48e00ee78e9d5fcc4846f7ee6077
Jetlonglim <133329143+Jetlonglim@users.noreply.github.com>
2024-06-28 02:02:20 +0800

    Update robot.h

--- robot.h
+++ robot.h
@@ -5,68 +5,73 @@
 #include <ctime>
 #include <string>
 
+
 using namespace std;
 
 class Robot //created a robot class to store
 {			// and initialise a few variables and their responses of commands
-private:
-	int steps;
-	int id;
-	int Lives =3;
-	int Kills = 0;
-	int posX, posY;
 
 protected:
 	string Name;
+    string type;
+	int Lives;
+	int Kills = 0;
+	int posX, posY;
+    int Respawns;
 
 public: 
-	Robot(string name, string x, string y, int kills){// Constructor Method
+	Robot(string name, int x, int y, int kills){// Constructor Method
 		Name = name;
-		posX = stoi(x);
-		posY = stoi(y);
-		Kills = kills;
+		posX =x;
+		posY = y;
+		Kills= kills;
+        Lives= 3;
+        Respawns = 3;
 	}
+    virtual void move(Robot* robots[], int mapX, int mapY) = 0;
+    virtual void interact(Robot* other, int mapX, int mapY) = 0; 
+    virtual string getType() const =0;
     virtual ~Robot() {}
-	int getPosX() const {
-        return posX;
-    }
-    int getPosY() const {
-        return posY;
-    }
-    bool isAlive() {
-        return Lives > 0;
-    }
-    void setAlive(bool alive) {
-    if (alive) {
-        Lives = 3; // Set lives to 3 when the robot is alive
-    } else {
-        Lives = 0; // Set lives to 0 when the robot dies
-    }
-}
-	void setPosX(int x) {
-        posX = x;
-    }
 
-    void setPosY(int y) {
-        posY = y;
-    }
+    string getName() const{ return Name; }
+	int getPosX() const { return posX;}
+    int getPosY() const { return posY;}
+    int getLives() const { return Lives; }
+    int getRespawn() const { return Respawns;}
+    int getKill() const { return Kills;}
+    bool isAlive() const { return Lives>0;}
+
+	void setPosX(int x) {posX = x;}
+    void setPosY(int y) {posY = y;}
+    void setAlive(bool alive) {
+        Lives = alive ? 3 : 0;
+    }   
 	void AddKill(){
 		Kills++;
 		cout << Name << " has killed an enemy. Total kills: " << Kills << endl;
 	}
-    int getKill() const {
-        return Kills;
-    }
-	virtual string getName() const {
-        return Name;
-    }
-    virtual string getType() const =0;
-		
+
     void resetPosition(int mapX, int mapY) {
         posX = rand() % mapX;
         posY = rand() % mapY;
     }
     
+	
+    void respawn() {
+        if(Respawns > 0) {
+            Lives = 3;
+            Respawns--;
+            cout << Name << " has respawned. Remaining respawns: " << Respawns << endl;
+        }
+    }
+    void takeDamage() {
+        if (Lives > 0) {
+            Lives--;
+            if (Lives == 0 && Respawns > 0) {
+                respawn();
+            }
+        }
+    }
 };
 
 #endif
